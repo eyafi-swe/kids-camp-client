2da76932f277bc01e0b79b6dfe87acd7cb216d3c
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-11-08 01:20:44 +0600

    Router and dynamic title check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Component/Home/Home';
+import Main from './Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      hello world
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import useTitle from '../../Hooks/useTitle';
+
+const Home = () => {
+    useTitle('KidsCamp');
+    return (
+        <div>
+            This is home
+        </div>
+    );
+};
+
+export default Home;
