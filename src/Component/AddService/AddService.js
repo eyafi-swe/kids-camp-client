899aaba20e45bb81dc5f0deef2febf8192e7d7dc
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-11-10 02:08:31 +0600

    Add service check

--- src/Component/AddService/AddService.js
+++ src/Component/AddService/AddService.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useTitle from '../../Hooks/useTitle';
 
 const AddService = () => {
 
+    useTitle('Add Service|Kids')
+    const [service,setService] = useState({});
+
     const handleAddService = event => {
         event.preventDefault();
+        console.log(service);
+        //http://localhost:5000/services
+        fetch('http://localhost:5000/services', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(service),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+                // const newShowenReviews = [...allReviews, data.review];
+                // setAllReviews(newShowenReviews);
+                // notify();
+                event.target.reset();
+                
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
 
+    const hanldeValue = event =>{
+        const field = event.target.name;
+        const value = event.target.value;
+        const newService = { ...service };
+        newService[field] = value;
+        setService(newService);
     }
 
     return (
@@ -12,47 +44,54 @@ const AddService = () => {
             <div>
                 <h1 className='text-3xl text-center font-semibold mb-2'>Add New Service To Kids Camp</h1><hr />
 
-                <div className='mt-5 w-3/4 mx-auto'>
+                <div className='mt-5 lg:w-3/4 mx-auto'>
                     <form className="card-body" onSubmit={handleAddService}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Title Of Service</span>
                             </label>
-                            <input type="text" name='title' placeholder="Title of service" className="input input-bordered focus:outline-none input-info" required />
+                            <input onChange={hanldeValue} type="text" name='title' placeholder="Title of service" className="input input-bordered focus:outline-none input-info" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Image URL</span>
                             </label>
-                            <input type="text" name='imageurl' placeholder="Image URL" className="input input-info input-bordered focus:outline-none" required />
+                            <input onChange={hanldeValue} type="text" name='image' placeholder="Image URL" className="input input-info input-bordered focus:outline-none" required />
+
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Service Fee</span>
+                            </label>
+                            <input onChange={hanldeValue} type="text" name='fee' placeholder="Service Fee" className="input input-info input-bordered focus:outline-none" required />
 
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Service Method</span>
                             </label>
-                            <input type="text" name='method' placeholder="Service Method (At Home/My Place)" className="input input-info input-bordered focus:outline-none" required />
+                            <input onChange={hanldeValue} type="text" name='method' placeholder="Service Method (At Home/My Place)" className="input input-info input-bordered focus:outline-none" required />
 
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Service Provider Name</span>
                             </label>
-                            <input type="text" name='tutor' placeholder="Service provider name" className="input input-info input-bordered focus:outline-none" required />
+                            <input onChange={hanldeValue} type="text" name='tutor' placeholder="Service provider name" className="input input-info input-bordered focus:outline-none" required />
 
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Rating</span>
                             </label>
-                            <input type="number" name='rating' placeholder="Average rating of service" className="input input-info input-bordered focus:outline-none" required />
+                            <input onChange={hanldeValue} type="number" name='rating' placeholder="Average rating of service" className="input input-info input-bordered focus:outline-none" required />
 
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Description</span>
                             </label>
-                            <textarea className="textarea textarea-info focus:outline-none w-full" placeholder="Description of service" name='description' required></textarea>
+                            <textarea onChange={hanldeValue} className="textarea textarea-info focus:outline-none w-full" placeholder="Description of service" name='description' required></textarea>
                         </div>
                         <div className="form-control mt-5">
                             <button className="btn bg-sky-600 border-none">Add Service</button>
